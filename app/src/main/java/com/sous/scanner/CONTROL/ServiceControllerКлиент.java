adafb620369580d665228b67101273f303e3dd92
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2023-02-14 11:55:43 +0300

    scanner----14.02.2023----11.55---setPreferredPhy(BluetoothDevice.PHY_LE_2M_MASK,BluetoothDevice.PHY_LE_2M_MASK,BluetoothDevice.PHY_LE_2M_MASK.zip

--- app/src/main/java/com/sous/scanner/CONTROL/ServiceControllerКлиент.java
+++ app/src/main/java/com/sous/scanner/CONTROL/ServiceControllerКлиент.java
@@ -532,6 +532,7 @@ public class ServiceControllerКлиент extends IntentService {
                     gatt =      bluetoothDevice.connectGatt(context, false, bluetoothGattCallback, BluetoothDevice.TRANSPORT_AUTO,BluetoothDevice.PHY_LE_2M_MASK,handler);
                     Log.d(this.getClass().getName(), "\n" + " bluetoothDevice" + bluetoothDevice);
                     gatt.requestConnectionPriority(BluetoothGatt.CONNECTION_PRIORITY_HIGH);
+                    gatt.setPreferredPhy(BluetoothDevice.PHY_LE_2M_MASK,BluetoothDevice.PHY_LE_2M_MASK,BluetoothDevice.PHY_LE_2M_MASK);
                     bluetoothDevice.createBond();
                     int bondstate = bluetoothDevice.getBondState();
                     Log.d(TAG, "Trying to write characteristic..., first bondstate " + bondstate);
