75c945c636493f7f2e1efb97cefde51ec45e0d7a
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2023-04-03 22:18:14 +0300

    DSU0-----03.04.2023----22.17----recycreview крутиться и вниз и вбок----.zip

--- app/src/main/java/com/dsy/dsu/Code_ForTABEL/MainActivity_Tabel_Single_People.java
+++ app/src/main/java/com/dsy/dsu/Code_ForTABEL/MainActivity_Tabel_Single_People.java
@@ -3509,8 +3509,9 @@ try{
                         viewПолучениеМатериалов = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_main_grid_for_tables_four_columns_in_mm_htc, parent, false);
                     } else {
                         // TODO: 03.04.2023 для MM
-                        viewПолучениеМатериалов = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_main_grid_for_tables_four_columns_in_mm, parent, false);
-                    }      
+                     ///   viewПолучениеМатериалов = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_main_grid_for_tables_four_columns_in_mm, parent, false);
+                        viewПолучениеМатериалов = LayoutInflater.from(parent.getContext()).inflate(R.layout.simple_for_single_tabel_mm, parent, false);
+                    }
                     
                     
                     
