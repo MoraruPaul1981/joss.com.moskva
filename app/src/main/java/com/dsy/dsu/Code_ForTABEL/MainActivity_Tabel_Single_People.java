b742c23553a9e3f0aaca3c8dfa125dc7db2aa526
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2023-04-16 20:12:03 +0300

    DSU----16.04.2023--20.111.zip

--- app/src/main/java/com/dsy/dsu/Code_ForTABEL/MainActivity_Tabel_Single_People.java
+++ app/src/main/java/com/dsy/dsu/Code_ForTABEL/MainActivity_Tabel_Single_People.java
@@ -1833,6 +1833,7 @@ if(МЕсяцТабелей ==5 || МЕсяцТабелей==6|| МЕсяцТа�
                             viewSingleTabel = LayoutInflater.from(parent.getContext()).inflate(R.layout.simple_for_single_tabel_mm_one_row, parent, false);
             if (myViewHolder!=null) {
                 switch (myViewHolder.getAbsoluteAdapterPosition()){
+                    case 6:
                     case 7:
                         viewSingleTabel = LayoutInflater.from(parent.getContext()).inflate(R.layout.simple_for_single_tabel_mm_last_row, parent, false);
                         break;
