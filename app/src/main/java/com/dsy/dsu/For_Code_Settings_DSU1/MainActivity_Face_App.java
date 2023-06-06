e8895b975f75d19b469fa38d3b132e0f46b7f3c2
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2023-06-06 11:51:15 +0300

    DSU--06.06.2023---11.50-----.zip

--- app/src/main/java/com/dsy/dsu/For_Code_Settings_DSU1/MainActivity_Face_App.java
+++ app/src/main/java/com/dsy/dsu/For_Code_Settings_DSU1/MainActivity_Face_App.java
@@ -149,7 +149,7 @@ public class MainActivity_Face_App extends AppCompatActivity {
 
 
             // TODO: 06.04.2023  ТЕСТ КОД для 1С
-         //   методДляТетсирования1С();
+            методДляТетсирования1С();
         } catch (Exception e) {
             e.printStackTrace();
             Log.e(this.getClass().getName(), "Ошибка " + e + " Метод :" + Thread.currentThread().getStackTrace()[2].getMethodName() + " Линия  :"
@@ -916,9 +916,14 @@ public class MainActivity_Face_App extends AppCompatActivity {
     }
     private void методДляТетсирования1С() {
         try{
-  new Class_Get_Json_1C(getApplicationContext(),"http://uat.dsu1.ru:55080/dds/hs/apidrp/objects")
+            Log.d(this.getClass().getName(), "   методДляТетсирования1С");
+
+
+
+
+/*  new Class_Get_Json_1C(getApplicationContext(),"http://uat.dsu1.ru:55080/dds/hs/apidrp/objects")
           .МетодОтправляемJSONОт1СТестирование("userapi","User4321api",new StringBuffer("[ {  \"id\" : null,  \"chosen_organization\" : 0,  \"current_table\" : 273,  \"date_update\" : \"2022-11-27 10:33:20.737\",  \"fullname\" : \"Общество с ограниченной ответственностью СОЮЗ АВТОДОР\",  \"inn\" : \"3711025287\",  \"kpp\" : \"371101001\",  \"name\" : \"ООО СОЮЗ АВТОДОР\",  \"user_update\" : 8,  \"uuid\" : 2} ]"));
-            Log.i(this.getClass().getName(), "    holder. ФдагЧтоУжеОдинРАзБылПервыйПроход ");
+            Log.i(this.getClass().getName(), "    holder. ФдагЧтоУжеОдинРАзБылПервыйПроход ");*/
     } catch (Exception e) {
         e.printStackTrace();
         Log.e(this.getClass().getName(), "Ошибка " + e + " Метод :" + Thread.currentThread().getStackTrace()[2].getMethodName() + " Линия  :"
