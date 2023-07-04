5f340811c0f43d6ca169b17336c76c66eafc3bce
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2023-07-04 09:44:35 +0300

    DSU---04,07,2023--9,44- 1СУдалено,6-.zip.zip

--- app/src/main/java/com/dsy/dsu/Code_For_Services/Service_For_Public.java
+++ app/src/main/java/com/dsy/dsu/Code_For_Services/Service_For_Public.java
@@ -688,11 +688,11 @@ public Cursor МетодПолучениеДанныхЧерезCursorLoader(@No
         // TODO: 25.09.2022 запуск метода
         public void МетодУдаленияСтатусаУдаленных(@NonNull Intent intent){
             try{
-                Stream<String> streamУдалениеСтатусаУдаленный=Stream.of("data_tabels","tabel","get_materials_data");
+                Stream<String> streamУдалениеСтатусаУдаленный=Stream.of("data_tabels","tabel","get_materials_data","order_tc");
                 streamУдалениеСтатусаУдаленный.forEach(Таблица->{
                     Uri  uri = Uri.parse("content://com.dsy.dsu.providerdatabase/" + Таблица + "");
                     ContentResolver resolver = context.getContentResolver();
-                    Integer  УдалениеДанныхСоСтатусомУдаленная=   resolver.delete(uri,"status_send=?",new String[]{"Удаленная"});
+                    Integer  УдалениеДанныхСоСтатусомУдаленная=   resolver.delete(uri,"status_send=? OR  status=?",new String[]{"1СУдалено","6"});
                     Log.d(this.getClass().getName(),"\n" + " class " + Thread.currentThread().getStackTrace()[2].getClassName() + "\n" +
                             " metod " + Thread.currentThread().getStackTrace()[2].getMethodName() + "\n" +
                             " line " + Thread.currentThread().getStackTrace()[2].getLineNumber() + "\n"+
