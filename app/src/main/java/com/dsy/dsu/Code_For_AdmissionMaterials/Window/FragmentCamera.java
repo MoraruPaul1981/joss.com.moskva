1cac0ccd11a422c91af2004521ceef19e1e3f864
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2023-08-02 10:06:09 +0300

    DSU-----02.08.2023---10.05--.zip

--- app/src/main/java/com/dsy/dsu/Code_For_AdmissionMaterials/Window/FragmentCamera.java
+++ app/src/main/java/com/dsy/dsu/Code_For_AdmissionMaterials/Window/FragmentCamera.java
@@ -1,11 +1,24 @@
 package com.dsy.dsu.Code_For_AdmissionMaterials.Window;
 
+import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.graphics.Camera;
 import android.os.Bundle;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.camera.core.CameraControl;
+import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageCapture;
+import androidx.camera.core.Preview;
+import androidx.camera.core.UseCaseGroup;
+import androidx.camera.core.VideoCapture;
+import androidx.camera.core.ViewPort;
 import androidx.camera.core.impl.VideoCaptureConfig;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.DialogFragment;
 
 import android.os.Environment;
@@ -20,8 +33,10 @@ import android.widget.ImageButton;
 import com.dsy.dsu.Business_logic_Only_Class.Class_Generation_Errors;
 import com.dsy.dsu.R;
 import com.google.android.material.button.MaterialButton;
+import com.google.common.util.concurrent.ListenableFuture;
 
 import java.io.File;
+import java.util.concurrent.ExecutionException;
 
 import javax.annotation.Nullable;
 
@@ -31,6 +46,9 @@ public class FragmentCamera extends DialogFragment {
     private ImageButton imageButtonCameraback;
     private MaterialButton button_create_new_image;
     private  BisinessLogica bisinessLogica;
+    private  PreviewView preview_view;
+    private ImageCapture imageCapture;
+    public static final int CAMERA_PERSSION_CODE=1;
 
     public FragmentCamera() {
         // Required empty public constructor
@@ -49,17 +67,7 @@ public class FragmentCamera extends DialogFragment {
         super.onCreate(savedInstanceState);
         try{
             bisinessLogica=new BisinessLogica();
-
-            // TODO: 02.08.2023  TEST CODE
-
-            File file = new File(Environment.getExternalStorageDirectory()+"/HeyThisISJayuir.jpg");
-
-            ImageCapture.OutputFileOptions outputFileOptions=new ImageCapture.OutputFileOptions.Builder(file).build();
-
-
-
-
-
+            bisinessLogica.    методДаемПраваНаCameraPermissions();
             // TODO: 20.07.2023
             Log.d(this.getClass().getName(),"\n" + " class " + Thread.currentThread().getStackTrace()[2].getClassName() + "\n" +
                     " metod " + Thread.currentThread().getStackTrace()[2].getMethodName() + "\n" +
@@ -86,6 +94,64 @@ public class FragmentCamera extends DialogFragment {
             
             button_create_new_image=(MaterialButton) viewRoot.findViewById(R.id.button_create_new_image);
             bisinessLogica.  методСлушатели();
+
+
+
+
+            // TODO: 02.08.2023  TEST CODE
+
+            File file = new File(Environment.getExternalStorageDirectory()+"/HeyThisISJayuir.jpg");
+
+            ImageCapture.OutputFileOptions outputFileOptions=new ImageCapture.OutputFileOptions.Builder(file).build();
+
+
+            // TODO: 02.08.2023  TEst Code Video
+
+            Preview preview = new Preview.Builder().build();
+            PreviewView preview_view =viewRoot. findViewById(R.id.preview_view);
+
+            bisinessLogica.    setCameraProviderListener();
+
+
+            // TODO: 20.07.2023
+            Log.d(this.getClass().getName(),"\n" + " class " + Thread.currentThread().getStackTrace()[2].getClassName() + "\n" +
+                    " metod " + Thread.currentThread().getStackTrace()[2].getMethodName() + "\n" +
+                    " line " + Thread.currentThread().getStackTrace()[2].getLineNumber() + "\n"   + " preview_view " +preview_view );
+
+
+/*// The use case is bound to an Android Lifecycle with the following code
+            Camera camera = cameraProvider.bindToLifecycle(lifecycleOwner, cameraSelector, preview);
+
+// PreviewView creates a surface provider, using a Surface from a different
+// kind of view will require you to implement your own surface provider.
+            preview. = viewFinder.getSurfaceProvider();*/
+
+
+
+
+
+            // TODO: 02.08.2023  TEst Code Video 2
+
+/*
+         VideoCapture videoCapture = VideoCaptureConfig.Builder()     // <--- Builder is in red
+                    .setTargetRotation(binding.previewView.display.rotation)
+                    .setCameraSelector(cameraSelector)
+                    .setTargetAspectRatio(screenAspectRatio)
+                    .build();*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             // TODO: 20.07.2023
             Log.d(this.getClass().getName(),"\n" + " class " + Thread.currentThread().getStackTrace()[2].getClassName() + "\n" +
                     " metod " + Thread.currentThread().getStackTrace()[2].getMethodName() + "\n" +
@@ -194,6 +260,69 @@ public class FragmentCamera extends DialogFragment {
                         this.getClass().getName().toString(), Thread.currentThread().getStackTrace()[2].getMethodName().toString(),
                         Thread.currentThread().getStackTrace()[2].getLineNumber());
             }
+        }
+        public  void методДаемПраваНаCameraPermissions(){
+            if (ContextCompat.checkSelfPermission(getActivity(),android. Manifest.permission.CAMERA ) != PackageManager.PERMISSION_GRANTED) {
+                // Permission is not granted
+                Log.d("checkCameraPermissions", "No Camera Permissions");
+                //////////////////////TODO SERVICE
+                String[] permissions = new String[]{
+                        android. Manifest.permission.CAMERA,
+                        android. Manifest.permission.RECORD_AUDIO,
+                        android.Manifest.permission.INTERNET,
+                        android.Manifest.permission.READ_PHONE_STATE,
+                        android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        android.Manifest.permission.VIBRATE,
+                        android.Manifest.permission.RECORD_AUDIO,
+                        android.Manifest.permission.REQUEST_INSTALL_PACKAGES,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION,
+                        android.Manifest.permission.ACCESS_LOCATION_EXTRA_COMMANDS,
+                        android.Manifest.permission.MANAGE_EXTERNAL_STORAGE,
+                        android.Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+                        android.Manifest.permission.ACCESS_NETWORK_STATE,
+                        android.Manifest.permission.ACCESS_MEDIA_LOCATION,
+                        android.Manifest.permission.INSTALL_PACKAGES,
+                        android.Manifest.permission.WRITE_SETTINGS,
+                        android. Manifest.permission.WRITE_SECURE_SETTINGS
+                };
+                ActivityCompat.requestPermissions(getActivity(), permissions, CAMERA_PERSSION_CODE);
+
+
+            }else{
+                // Permission is not granted
+                Log.d("checkCameraPermissions", "Success YRA  Camera Permissions  !!!!");
+            }
+        }
+
+        private void setCameraProviderListener() {
+            ListenableFuture<ProcessCameraProvider> cameraProviderFuture =
+                    ProcessCameraProvider.getInstance(requireContext());
+            cameraProviderFuture.addListener(() -> {
+
+                try {
+                    ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                    bindPreview(cameraProvider);
+                } catch (ExecutionException | InterruptedException e) {
+                    // No errors need to be handled for this Future
+                    // This should never be reached
+                    e.printStackTrace();
+                }
+            }, ContextCompat.getMainExecutor(requireContext()));
+        }
+
+        private void bindPreview(ProcessCameraProvider cameraProvider) {
+
+            CameraSelector cameraSelector =
+                    new CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
+
+            Preview preview = new Preview.Builder().build();
+
+            preview.setSurfaceProvider(preview_view.getSurfaceProvider());
+
+            ViewPort viewPort = preview_view.getViewPort();
+
+
         }
 
 
