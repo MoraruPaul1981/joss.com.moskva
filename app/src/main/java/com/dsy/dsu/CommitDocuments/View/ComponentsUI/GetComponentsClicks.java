e6e904e4404ffcaf8f3fc138ababd35621b41afd
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2024-03-06 08:59:59 +0300

    android  mart2024

---
+++ app/src/main/java/com/dsy/dsu/CommitDocuments/View/ComponentsUI/GetComponentsClicks.java
@@ -0,0 +1,19 @@
+package com.dsy.dsu.CommitDocuments.View.ComponentsUI;
+
+import android.app.Activity;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+
+import com.dsy.dsu.CommitDocuments.View.ViewModel.FragmentCommitDocuments;
+
+/**
+ * Компоненты Фрагмента Согласование Документов КЛИК
+ */
+public class GetComponentsClicks extends GetComponentsUI {
+
+
+  public GetComponentsClicks(@NonNull View view, @NonNull Activity activity) {
+    super(view, activity);
+  }
+}
