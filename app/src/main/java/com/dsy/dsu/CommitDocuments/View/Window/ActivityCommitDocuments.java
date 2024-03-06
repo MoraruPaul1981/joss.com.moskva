e6a34df9ea9e617266c84a1e53f21b80545f0f5a
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2024-03-06 10:00:34 +0300

    android  mart2024

---
+++ app/src/main/java/com/dsy/dsu/CommitDocuments/View/Window/ActivityCommitDocuments.java
@@ -0,0 +1,23 @@
+package com.dsy.dsu.CommitDocuments.View.Window;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+
+import com.dsy.dsu.R;
+
+public class ActivityCommitDocuments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_commit_documents);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, FragmentCommitDocuments.newInstance())
+                    .commitNow();
+        }
+    }
+}
