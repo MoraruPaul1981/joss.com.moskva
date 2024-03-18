0c5c936c31c70e541e8eb2413448e64c49c07204
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2024-03-18 15:46:29 +0300

    android  mart2024

--- app/src/main/java/com/dsy/dsu/Hilt/Adress1cPays/ModuleCommintgPays.java
+++ app/src/main/java/com/dsy/dsu/Hilt/Adress1cPays/ModuleCommintgPays.java
@@ -27,10 +27,10 @@ public class ModuleCommintgPays {
         try {
 
             // TODO: 18.03.2024 Дебаг
-            АдресСеврера1сДляgetFile="http://192.168.3.10/dds_copy/hs/jsonto1c/listofdocuments".trim();// TODO: 18.01.2024 DEBUG
+           // АдресСеврера1сДляgetFile="http://192.168.3.10/dds_copy/hs/jsonto1c/listofdocuments".trim();// TODO: 18.01.2024 DEBUG
 
             // TODO: 18.03.2024  релиз
-          /// АдресСеврера1сДляgetFile="http://uat.dsu1.ru:55080/dds/hs/jsonto1c/listofdocuments".trim();// TODO: 18.01.2024  REliz
+           АдресСеврера1сДляgetFile="http://uat.dsu1.ru:55080/dds/hs/jsonto1c/listofdocuments".trim();// TODO: 18.01.2024  REliz
 
 
             Log.d(this.getClass().getName(), "\n" + " class " + Thread.currentThread().getStackTrace()[2].getClassName() + "\n" +
