88d13a4ef05dfdd8c3aa9d4e0222b758181e0874
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2024-04-18 12:50:51 +0300

    commintprices----17.04.2024------10.46----.zip

--- android/app/src/main/java/com/comming/commintprices/MVP/View/Widgets/WidgetListViewCommingPrices.dart
+++ android/app/src/main/java/com/comming/commintprices/MVP/View/Widgets/WidgetListViewCommingPrices.dart
@@ -14,16 +14,16 @@ class WidgetListViewCommingPrices extends State<StatefulWidgetCommingPrices>  {
 
     List<Person1CListManual> listManual=[
     //TODO
-    person1cListManual.loopGeneratorListPolo(CFOKey: 'CFOKey1', UUIDKey: 1111),
-    person1cListManual.loopGeneratorListPolo(CFOKey: 'CFOKey2', UUIDKey: 2222),
-    person1cListManual.loopGeneratorListPolo(CFOKey: 'CFOKey3', UUIDKey: 3333),
-    person1cListManual.loopGeneratorListPolo(CFOKey: 'CFOKey4', UUIDKey: 4444),
-    person1cListManual.loopGeneratorListPolo(CFOKey: 'CFOKey5', UUIDKey: 5555),
-    person1cListManual.loopGeneratorListPolo(CFOKey: 'CFOKey6', UUIDKey: 6666),
-    person1cListManual.loopGeneratorListPolo(CFOKey: 'CFOKey7', UUIDKey: 7777),
-    person1cListManual.loopGeneratorListPolo(CFOKey: 'CFOKey8', UUIDKey: 8888),
-    person1cListManual.loopGeneratorListPolo(CFOKey: 'CFOKey9', UUIDKey: 9999),
-    person1cListManual.loopGeneratorListPolo(CFOKey: 'CFOKey10', UUIDKey: 10000),
+    person1cListManual.loopGeneratorListPolo(CFOKey: '(Закрыт) Ремонт а/д Ковров-Шуя-Кинешма (1этап)', UUIDKey: 1111),
+    person1cListManual.loopGeneratorListPolo(CFOKey: '(закрыт) Воссст. изн-х слоев а/д Иваново-Родники (45+338 - 46+003) №909 от 12.09.2018               ', UUIDKey: 2222),
+    person1cListManual.loopGeneratorListPolo(CFOKey: '(закрыт) Ганантийный ремонт Белино-Михайловское (объект с того года)                                ', UUIDKey: 3333),
+    person1cListManual.loopGeneratorListPolo(CFOKey: '(Закрыт) Механизированная уборка и содержание дорог г.Фурманов №1 от 1.01.2018                      ', UUIDKey: 4444),
+    person1cListManual.loopGeneratorListPolo(CFOKey: '(Закрыт) Работы по устройству слоёв износа а/д М5"Урал" Челябинская обл. №101 от 25.06.2018         ', UUIDKey: 5555),
+    person1cListManual.loopGeneratorListPolo(CFOKey: '(Закрыт) Ремонт а/д Устюжно-Валдай 741751 от 07.08.17                                               ', UUIDKey: 6666),
+    person1cListManual.loopGeneratorListPolo(CFOKey: '(закрыт) Содержание 2018-2019 Лежневского и Ивановского р-нов (Григорьев Алексей)                   ', UUIDKey: 7777),
+    person1cListManual.loopGeneratorListPolo(CFOKey: '(Закрыт) Содержание а/д Р600 Кострома-Иваново №450 от 19.12.16 (с ноября закрыт)                    ', UUIDKey: 8888),
+    person1cListManual.loopGeneratorListPolo(CFOKey: 'Уборка города в рамках подготовки к выборам (Благотворительность)                                   ', UUIDKey: 9999),
+    person1cListManual.loopGeneratorListPolo(CFOKey: 'АБЗ первый, второй (Михалевский АБЗ) ПЕРЕВОЗКА МАТЕРИАЛОВ                                           ', UUIDKey: 10000),
     ];
 
 
@@ -33,7 +33,7 @@ class WidgetListViewCommingPrices extends State<StatefulWidgetCommingPrices>  {
         backgroundColor: Colors.grey[200],
         appBar:AppBar(
       backgroundColor: Colors.blue[300],
-          title: Text('Выбор а локации'),
+          title: Text('Выбор цфо'),
           centerTitle: true,
           elevation: 500,
     ),
@@ -44,7 +44,15 @@ class WidgetListViewCommingPrices extends State<StatefulWidgetCommingPrices>  {
             return Card(
               child: ListTile (
                 onTap: () {},
-                title: Text(listManual[index].CFO.toString()),
+                title: Text(listManual[index].CFO.toString().trim(),
+                  style: TextStyle(height:2,fontSize: 13,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Hind',
+                      color: Colors.black.withOpacity(0.7),
+                    decoration: TextDecoration.underline,
+                    decorationColor: Colors .blue[300],
+                    decorationStyle: TextDecorationStyle.wavy,) ,
+                ),
               ),
             );
           },
