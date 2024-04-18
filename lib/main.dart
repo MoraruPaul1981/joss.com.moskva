3739f758ec2910a99123ebf3d6ea11da722afeed
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2024-04-18 16:47:38 +0300

    flutter  aprel 2024

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+
+Future<void> main()  async {
+  try {
+    //TODO get LOGGER int
+print('Future<void> main()');
+  }   catch (e, stacktrace) {
+    print(' get ERROR $e get stacktrace $stacktrace ');
+  }
+}
