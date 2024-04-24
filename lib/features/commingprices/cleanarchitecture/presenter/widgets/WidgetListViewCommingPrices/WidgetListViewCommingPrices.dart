c00f6af0805c274c1db1415a7ef41c6d4d22ffde
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2024-04-24 17:01:48 +0300

    flutter april

--- lib/features/commingprices/cleanarchitecture/presenter/widgets/WidgetListViewCommingPrices/WidgetListViewCommingPrices.dart
+++ lib/features/commingprices/cleanarchitecture/presenter/widgets/WidgetListViewCommingPrices/WidgetListViewCommingPrices.dart
@@ -297,7 +297,7 @@ await Future.delayed(Duration(seconds: 1))
   Widget getWidgetScaffold(List<Entities1CListManual> listManual){
     ////TODO сам виджет
     return new Scaffold(
-      backgroundColor: Colors.grey[200],
+      backgroundColor: Colors.blue[200],
       appBar: AppBar(
         leading: new Icon(Icons.live_tv),
         backgroundColor: Colors.blue[300],
@@ -306,12 +306,11 @@ await Future.delayed(Duration(seconds: 1))
         elevation: 500,
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
+
+
         children: <Widget>[
-
-
-
           Row(
             children: [
               Expanded(
@@ -360,83 +359,15 @@ await Future.delayed(Duration(seconds: 1))
             ],
           ),
 
-          Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(2.0),
-                  child:                  ListView.builder(
-                        shrinkWrap: true,
-                        primary: false,
-                        scrollDirection:
-                        Axis.vertical, // Axis.horizontal for horizontal list view.
-                        itemCount: listManual.length,
-                        itemBuilder: (context, index) {
-                        final user = listManual[index].CFO.toString().trim();
-                        final UUID = listManual[index].UUID.toString().trim();
-                        //TODO
-                        return Column(
-                        children: [
-                        Card(
-                        color: Colors.grey[100],
-                        elevation: 1.0,
-                        shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                        child: ListTile(
-                        leading: const Icon(Icons.flight_sharp),
-                        subtitle: Text(""),
-                        trailing: Icon(Icons.more_vert),
-                        onTap: () {
-                        print('object');
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: Text(user.trim().toLowerCase() +
-                        "\n" +
-                        "uuid-> " +
-                        UUID),
-                        ));
-                        },
-                        title: SizedBox(
-                        height: 45,
-                        child: Center(
-                        child: Text(
-                        listManual[index].CFO.toString().trim(),
-                        style: TextStyle(
-                        height: 2,
-                        fontSize: 13,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black.withOpacity(0.7),
-                        decoration: TextDecoration.underline,
-                        decorationColor: Colors.blue[300],
-                        decorationStyle: TextDecorationStyle.wavy,
-                        ),
-                        ),
-                        ),
-                        ),
-                        ),
-                        ),
-                        ],
-                        );
-                        },
-                        ),
-                ),
-              ),
-            ],
-          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(2.0),
+                child: ConstrainedBox(
+                  constraints: new BoxConstraints(
+                    minHeight: 550.0,
+                    maxHeight: 700.0,
+                  ),
 
-
-
-
-
-
-
-
-
-
-
-
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(3.0),
                   child: ListView.builder(
                     shrinkWrap: true,
                     primary: false,
@@ -491,23 +422,29 @@ await Future.delayed(Duration(seconds: 1))
                     },
                   ),
                 ),
-              ),
-
+            ),
+          ),
         ],
       ),
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.all(3.0),
-        child: FloatingActionButton.extended(
-          elevation: 20.0,
-          icon: const Icon(Icons.person_add),
-          label: const Text('Цфо'),
-          backgroundColor: Colors.blue[300],
-          onPressed: () {
-            /* Navigator.push(
-              context,
-              MaterialPageRoute(builder: (_) => CameraScreen()),
-            );*/
-          },
+
+
+
+//TODO  кенопка снизу
+      floatingActionButton: Expanded(
+        child: Padding(
+          padding: const EdgeInsets.all(3.0),
+          child: FloatingActionButton.extended(
+            elevation: 20.0,
+            icon: const Icon(Icons.person_add),
+            label: const Text('Цфо'),
+            backgroundColor: Colors.blue[300],
+            onPressed: () {
+              /* Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => CameraScreen()),
+              );*/
+            },
+          ),
         ),
       ),
     );
