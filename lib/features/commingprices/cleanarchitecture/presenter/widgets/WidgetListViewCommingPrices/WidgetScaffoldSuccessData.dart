1940370aa3617010819275eae871600b5af72c21
Ferrari1981 <xiaomimoraru.part17@gmail.com>
2024-04-25 09:42:41 +0300

    flutter april

--- lib/features/commingprices/cleanarchitecture/presenter/widgets/WidgetListViewCommingPrices/WidgetScaffoldSuccessData.dart
+++ lib/features/commingprices/cleanarchitecture/presenter/widgets/WidgetListViewCommingPrices/WidgetScaffoldSuccessData.dart
@@ -30,44 +30,14 @@ class   WidgetScaffoldSuccessData {
 
 
         children: <Widget>[
+          //TODO строчки с обьектами
           Row(
             children: [
               Expanded(
                 child: Padding(
                   padding: const EdgeInsets.all(2.0),
-                  child: SizedBox(
-                    height: 35,
-                    child: SearchBar(
-                      hintText: 'Поиск',
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-
-          Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(2.0),
-                  child: SizedBox(
-                    height: 35,
-                    child: SearchBar(
-                      hintText: 'Поиск',
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-
-          Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(2.0),
-                  child: SizedBox(
+                  child: Container(
+                    margin: new EdgeInsets.only( left: 5,top:2,right: 5,bottom: 2),
                     height: 35,
                     child: SearchBar(
                       hintText: 'Поиск',
@@ -104,32 +74,38 @@ class   WidgetScaffoldSuccessData {
                           elevation: 1.0,
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(20)),
-                          child: ListTile(
-                            leading: const Icon(Icons.flight_sharp),
-                            subtitle: Text(""),
-                            trailing: Icon(Icons.more_vert),
-                            onTap: () {
-                              print('object');
-                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                                content: Text(user.trim().toLowerCase() +
-                                    "\n" +
-                                    "uuid-> " +
-                                    UUID),
-                              ));
-                            },
-                            title: SizedBox(
-                              height: 45,
-                              child: Center(
-                                child: Text(
-                                  listManual[index].CFO.toString().trim(),
-                                  style: TextStyle(
-                                    height: 2,
-                                    fontSize: 13,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black.withOpacity(0.7),
-                                    decoration: TextDecoration.underline,
-                                    decorationColor: Colors.blue[300],
-                                    decorationStyle: TextDecorationStyle.wavy,
+                          child: ConstrainedBox(
+                            constraints: new BoxConstraints(
+                                minHeight: 20.0,
+                                maxHeight: 300.0,),
+                            child: ListTile(
+                              leading: const Icon(Icons.flight_sharp),
+                              subtitle: Text(""),
+                              trailing: Icon(Icons.more_vert),
+                              onTap: () {
+                                print('object');
+                                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                  content: Text(user.trim().toLowerCase() +
+                                      "\n" +
+                                      "uuid-> " +
+                                      UUID),
+                                ));
+                              },
+                              title:
+                              SizedBox(
+                                height: 45,
+                                child: Center(
+                                  child: Text(
+                                    listManual[index].CFO.toString().trim(),
+                                    style: TextStyle(
+                                      height: 2,
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black.withOpacity(0.7),
+                                      decoration: TextDecoration.underline,
+                                      decorationColor: Colors.blue[300],
+                                      decorationStyle: TextDecorationStyle.wavy,
+                                    ),
                                   ),
                                 ),
                               ),
@@ -154,7 +130,7 @@ class   WidgetScaffoldSuccessData {
       children: [
         Expanded(
         child: Container(
-          margin: new EdgeInsets.only( left: 2,top: 0,right: 2,bottom: 0),
+          margin: new EdgeInsets.only( left: 5,top: 0,right: 5,bottom: 0),
           height: 60,
           clipBehavior: Clip.hardEdge,
           // color: Colors.red,
@@ -195,11 +171,10 @@ class   WidgetScaffoldSuccessData {
                 ),
                 NavigationDestination(
                   selectedIcon: Icon(Icons.accessibility_new),
-                  icon: Icon(Icons.accessibility_new_outlined),
+                  icon: Icon(Icons.accessibility_new_sharp),
                   label: 'Начисления',
                 ),
               ],
-
             ),
 
         ),
